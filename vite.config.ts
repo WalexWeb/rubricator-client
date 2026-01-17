8edc156d221c882a2877ab5d9ea78acdb3f3211a
WalexWeb <spomeshchikov@yandex.ru>
2026-01-17 11:33:48 +0300

    perf: обновил конфиг сборщика

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +15,10 @@ export default defineConfig({
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:3800",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
